5dccf4b11d62ff540d9a47bfb55ff3df4273a5d9
Thoyib Antarnusa <tybantarnusa@null.net>
2023-12-03 21:23:11 +0700

    Day 3 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,5 +1,39 @@
 use std::{fs::File, error::Error, io::BufReader, io::BufRead};
 
+struct Node {
+    value: i32,
+    left: usize,
+    right: usize,
+    bottom: usize,
+    top: usize,
+}
+
+impl Node {
+    fn new_num(_rows: &Vec<String>, symbol: String, index: usize, row: usize) -> Node {
+        Node {
+            value: symbol.clone().parse().unwrap_or(0),
+            left: index,
+            right: index+symbol.len()-1,
+            bottom: row,
+            top: row,
+        }
+    }
+
+    fn new_gear(rows: &Vec<String>, symbol: String, index: usize, row: usize) -> Node {
+        Node {
+            value: symbol.clone().parse().unwrap_or(0),
+            left: if index != 0 { index-1 } else { 0 },
+            right: if index+symbol.len() != rows.len() { index+1 } else { rows.len()-1 },
+            bottom: if row != rows.len()-1 { row+1 } else { rows.len()-1 },
+            top: if row != 0 { row-1 } else { 0 },
+        }
+    }
+
+    fn collide(&self, other: &Node) -> bool {
+        self.left <= other.right && self.right >= other.left && self.top <= other.bottom && self.bottom >= other.top
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>>{
     let file = File::open("input")?;
     let reader = BufReader::new(file);
@@ -12,6 +46,9 @@ fn main() -> Result<(), Box<dyn Error>>{
         rows.push(line);
     }
 
+    let mut num_list = Vec::<Node>::new();
+    let mut gear_list = Vec::<Node>::new();
+
     for (i_row, row) in rows.iter().enumerate() {
         let mut suspected_number = String::new();
         let mut suspected_idx: isize = -1;
@@ -23,12 +60,38 @@ fn main() -> Result<(), Box<dyn Error>>{
                 }
             }
             if suspected_number.len() > 0 && (!ch.is_digit(10) || i == row.len() - 1) {
-                if check_surround(&rows, i_row, suspected_idx as usize, suspected_number.len()) {
-                    sum += suspected_number.parse::<usize>()?;
-                }
+                num_list.push(Node::new_num(&rows, suspected_number.clone(), suspected_idx as usize, i_row));
                 suspected_number = String::new();
                 suspected_idx = -1;
             }
+            if ch == '*' {
+                gear_list.push(Node::new_gear(&rows, "*".to_owned(), i, i_row));
+            }
+        }
+    }
+
+    for gear in gear_list {
+        let mut pair = Vec::<&Node>::new();        
+        for num in num_list.iter() {
+            if num_list.is_empty() {
+                break;
+            }
+
+            if pair.len() == 2 {
+                break;
+            }
+            
+            if num.top > gear.bottom {
+                break;
+            }
+
+            if gear.collide(&num) {
+                pair.push(num);
+            }
+        }
+
+        if pair.len() == 2 {
+            sum += pair[0].value * pair[1].value;
         }
     }
 
@@ -36,52 +99,3 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     Ok(())
 }
-
-fn check_surround(rows: &Vec<String>, row: usize, col: usize, len: usize) -> bool {
-    if row != 0 {
-        let row_check = row - 1;
-        for i in col..col+len {
-            let symbol_test = &rows[row_check].chars().collect::<Vec<char>>()[i];
-            if !symbol_test.is_digit(10) && *symbol_test != '.' {
-                return true;
-            }
-        }
-    }
-
-    if row != rows.len() - 1 {
-        let row_check = row + 1;
-        for i in col..col+len {
-            let symbol_test = &rows[row_check].chars().collect::<Vec<char>>()[i];
-            if !symbol_test.is_digit(10) && *symbol_test != '.' {
-                return true;
-            }
-        }
-    }
-
-    if col != 0 {
-        let col_check = col - 1;
-        let start_row = if row != 0 { row - 1 } else { row };
-        let end_row = if row != rows.len()-1 { row + 1 } else { row };
-        for i in start_row..=end_row {
-            let symbol_test = &rows[i].chars().collect::<Vec<char>>()[col_check];
-            if !symbol_test.is_digit(10) && *symbol_test != '.' {
-                return true;
-            }
-        }
-    }
-
-    if col + len != rows[0].len() {
-        let col_check = col + len;
-        let start_row = if row != 0 { row - 1 } else { row };
-        let end_row = if row != rows.len()-1 { row + 1 } else { row };
-        for i in start_row..=end_row {
-            let symbol_test = &rows[i].chars().collect::<Vec<char>>()[col_check];
-            if !symbol_test.is_digit(10) && *symbol_test != '.' {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-        
